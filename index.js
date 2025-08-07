2d1c8b03f6a3428a98fd8e2de2ddc2385e7a6b9b
rithsombo <140787566+rithsombo@users.noreply.github.com>
2025-08-07 11:25:59 +0700

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/helloworld', (req, res) => {
   res.json('Hello World!');
 });
 
